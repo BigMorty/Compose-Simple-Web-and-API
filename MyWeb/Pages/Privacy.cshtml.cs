2dd10c50ebbd0173f91696a972956b0e46b1fbb3
Mike Morton <mikemort@microsoft.com>
2019-03-07 14:37:28 -0800

    Add project files.

---
+++ MyWeb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWeb.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            var httpClient = new HttpClient();
+            using (HttpResponseMessage response = await httpClient.GetAsync(new Uri("http://mywebapi/api/values/1")))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    ViewData["Message"] = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    ViewData["Message"] = "Call failed to 'MyAPI'";
+                }
+            }
+
+            return Page();
+        }
+    }
+}
